abdab0cad1546c7454c0c4caab1a4b6b85653a6a
Gabriel Mororó <gabrielmororo.gl@gmail.com>
2025-06-02 23:42:27 -0300

    calculadora 2

---
+++ CALCULADORA_PROG/compiler.bat
@@ -0,0 +1,19 @@
+@echo off
+set TESTBENCH=processador_tb
+
+:: Compila todos os arquivos VHDL
+ghdl -a *.vhd
+
+:: Compila o testbench
+ghdl -a %TESTBENCH%.vhd
+ghdl -e %TESTBENCH%
+
+:: Executa a simulação e gera o arquivo de waveform
+ghdl -r %TESTBENCH% --wave=%TESTBENCH%.ghw
+
+:: Abre o GTKWave
+gtkwave %TESTBENCH%.ghw
+
+:: Limpa arquivos gerados
+del /f /q work-obj93.cf
+del /f /q %TESTBENCH%.ghw
